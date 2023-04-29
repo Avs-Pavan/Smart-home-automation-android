c35d28f8ad1b6665917465e68bdd6ab15409faa1
kevinocconer <developer.kevin11@gmail.com>
2023-04-28 23:45:36 -0500

    Sending mqtt message after validation completed

--- app/src/main/java/com/example/shas/App.kt
+++ app/src/main/java/com/example/shas/App.kt
@@ -1,11 +1,29 @@
 package com.example.shas
 
 import android.app.Application
+import com.amazonaws.auth.CognitoCachingCredentialsProvider
+import com.amazonaws.mobile.client.AWSMobileClient
+import com.amazonaws.regions.Regions
+import com.kevin.devil.Devil
+import com.kevin.devil.models.DevilConfig
 import timber.log.Timber
 
-class App :Application() {
+class App : Application() {
+
+    private val serverUri = "tcp://142.93.57.145:1883"
+
     override fun onCreate() {
         super.onCreate()
+
+        val credentialsProvider: CognitoCachingCredentialsProvider =
+            CognitoCachingCredentialsProvider(
+                applicationContext,
+                "us-east-2:5f4f5852-cc94-435c-848e-fc12dddbf9cb",  // Identity pool ID
+                Regions.US_EAST_2 // Region
+            )
+
+        AWSMobileClient.getInstance().credentialsProvider = credentialsProvider
+
         if (BuildConfig.DEBUG) {
             Timber.plant(object : Timber.DebugTree() {
                 override fun createStackElementTag(element: StackTraceElement): String {
@@ -13,5 +31,17 @@ class App :Application() {
                 }
             })
         }
+
+//        Devil.breath(
+//            DevilConfig(
+//                true,
+//                true,
+//                serverUri,
+//                applicationContext,
+//                "Tag",
+//                "Will_topic",
+//                "123456"
+//            )
+//        )
     }
 }
